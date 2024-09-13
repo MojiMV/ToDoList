23ff68240ded4e08843a24ad23ceb1e4ec116fad
moji <mojtabavahabnezhad@gmail.com>
2024-09-13 12:24:16 +0330

    create the toDoList module

--- src/toDoList.js
+++ src/toDoList.js
@@ -1,26 +1,45 @@
-let projects = [];
+const toDoList = function(){
+    let projects = [];
 
-function createProject(name){
+    function addProject(name){
+        if (projects.some(project => project.name == name)){
+            console.log("This project name already exists!");
+            return;
+        }
+
+        let newProject = Project(name);
+        projects.push(createProject(name));
+    }
+
+    function removeProject(name){
+        let projectIndex = projects.findIndex(project => project.name == name);
+
+        if ( projectIndex == -1){
+            console.log("Not found!");
+            return;
+        }
+        projects.splice(projectIndex ,1);
+    }
+
+    function changeProjectName(previousName, newName){
+        if (projects.some(project => project.name === newName)) {
+            console.log(`Project name "${newName}" already exists. Choose a different name.`);
+            return;
+        }
+
+        projects.forEach(project => {
+            if (project.name == previousName){
+                project.name = newName;
+                return;
+            }
+        })
+    }
+
     return{
-        name: name,
+        addProject,
+        removeProject,
+        changeProjectName
     }
 }
 
-function addProject(name){
-    if (projects.some(project => project.name == name)){
-        console.log("This project name already exists!");
-        return;
-    }
-
-    projects.push(createProject(name));
-}
-
-function removePorject(name){
-    let projectIndex = projects.findIndex(project => project.name == name);
-
-    if ( projectIndex == -1){
-        console.log("Not found!");
-        return;
-    }
-    projects.splice(projectIndex ,1);
-}
+export default toDoList;
